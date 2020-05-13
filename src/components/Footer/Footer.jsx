6224344345dda080fa85e3e93845c0ae4e4f5168
Hadar Alon <hadaralon3@gmail.com>
2020-05-13 14:59:03 +0300

    update fade animation

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import hadar from "../../images/hadar.png"
+import db from "../../images/db.svg"
+
 import Fade from 'react-reveal/Fade';
 
 const Footer = props => {
@@ -9,6 +11,7 @@ const Footer = props => {
             <Fade bottom>
                  <img className='hadar__logo' src={hadar} alt="logo" />
             </Fade>
+            <img className='db__logo' src={db} alt="db_logo" />
         </footer>
     );
 };
